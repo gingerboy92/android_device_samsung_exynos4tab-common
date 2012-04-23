68b5a5065165a36e16e1b28478e5be47a363a307
R. Andrew Ohana <andrew.ohana@gmail.com>
2012-04-23 15:06:39 -0700

    few edits to the inline modules maketarget from J. Deitch

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -111,8 +111,8 @@ WIFI_DRIVER_MODULE_NAME          := ath6kl
 TARGET_KERNEL_SOURCE := kernel/samsung/c210
 EXTRA_MODULES:
 	cd hardware/compat-wireless; ./scripts/driver-select ath6kl
-	make -C hardware/compat-wireless KLIB=$(KERNEL_SRC) KLIB_BUILD=$(KERNEL_OUT) ARCH="arm" CROSS_COMPILE="arm-eabi-"
-	make -C hardware/compat-wireless KLIB=$(KERNEL_SRC) KLIB_BUILD=$(KERNEL_OUT) ARCH="arm" CROSS_COMPILE="arm-eabi-" install-modules
+	export CROSS_COMPILE=$(ARM_EABI_TOOLCHAIN)/arm-eabi-; $(MAKE) -C hardware/compat-wireless KLIB=$(KERNEL_SRC) KLIB_BUILD=$(KERNEL_OUT) ARCH=$(TARGET_ARCH) $(ARM_CROSS_COMPILE)
+	export CROSS_COMPILE=$(ARM_EABI_TOOLCHAIN)/arm-eabi-; (MAKE) -C hardware/compat-wireless KLIB=$(KERNEL_SRC) KLIB_BUILD=$(KERNEL_OUT) ARCH=$(TARGET_ARCH) $(ARM_CROSS_COMPILE) install-modules
 	cp `find $(KERNEL_OUT)/$(TARGET_KERNEL_SOURCE) -name *.ko` $(KERNEL_MODULES_OUT)/
 	arm-eabi-strip --strip-debug `find $(KERNEL_MODULES_OUT) -name *.ko`
 
